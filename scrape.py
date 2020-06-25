365ec7d185a4e53941d98b4d60c00a3194d556ad
fredlc <fredlefevre@outlook.com>
2020-06-25 13:07:04 -0400

    Initial commit

---
+++ scrape.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://news.ycombinator.com/')
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.storylink')
+votes = soup.select('.score')
+print(links[0])
+print(votes[0])
